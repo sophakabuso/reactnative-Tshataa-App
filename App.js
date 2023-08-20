921d8b39ec7fcf70272c49e35975115eef8a609f
Sophakabuso <60679360@mylife.unisa.ac.za>
2023-08-21 01:38:31 +0200

    stk nav

--- App.js
+++ App.js
@@ -4,15 +4,31 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './pages/LoginScreen';
 import RegisterScreen from './pages/RegisterScreen';
+import {usefonts} from 'expo-font';
+import HomeScreen from './pages/HomeScreen';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
 export default function App() {
   const Stack = createNativeStackNavigator();
-
+  const Tab = createBottomTabNavigator();
+  //usefonts({
+ //  'berkshire':require('./assets/fonts/berkshire.ttf')
+ // })
+ 
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Register" component={RegisterScreen} />
+      
+        <Stack.Screen name="Login" options={{
+          headerShown: false
+        }}>
+          {(props)=><LoginScreen {...props} />}
+         </Stack.Screen>
+          <Stack.Screen name="Register" options={{
+         headerShown: false
+          }}  >
+          {(props)=><RegisterScreen {...props} />}
+        </Stack.Screen>  
       </Stack.Navigator>
     </NavigationContainer>
   );
